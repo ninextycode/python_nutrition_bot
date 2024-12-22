8139eb90d3e4eba80ba75d62574b095783a610e4
ninexty <maxim@maxim-lenovo>
2024-12-22 01:20:32 +0000

    add food database classes

--- database/food_database_model.py
+++ database/food_database_model.py
@@ -1,13 +1,25 @@
 from typing import List, Optional
-
-from sqlalchemy import CheckConstraint, DECIMAL, Date, DateTime, ForeignKeyConstraint, Index, String, text
+from sqlalchemy import (
+    CheckConstraint, ForeignKeyConstraint, MetaData,
+    Date, DateTime, Index, String, text
+)
 from sqlalchemy.dialects.mysql import DECIMAL, INTEGER, TINYINT
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 import datetime
 import decimal
 
+
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention=naming_convention)
 
 
 class Genders(Base):
